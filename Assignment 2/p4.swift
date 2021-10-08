ceb940806e3da994774648fe690917179a18407a
fenil292 <65783083+fenil292@users.noreply.github.com>
2021-10-08 22:23:12 +0530

    Update p4.swift

--- Assignment 2/p4.swift
+++ Assignment 2/p4.swift
@@ -1,1 +1,14 @@
-
+var n=5,l=1
+for i in 0..<(n*2)-1{
+  for j in 0..<n+l{
+    if(j<=n-1-i || (i>n-1 && j<2+(i-n))){
+      print("  ",terminator:"")
+    }
+    else{
+      print("* ",terminator:"")
+    }
+  }
+  if(i<n-1){ l+=1 }
+  else{ l-=1 }
+  print()
+}
