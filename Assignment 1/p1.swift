3cdda6c51a0eb235b8259cdde734940fbde211ea
fenil292 <65783083+fenil292@users.noreply.github.com>
2021-10-09 16:56:15 +0530

    Update p1.swift

--- Assignment 1/p1.swift
+++ Assignment 1/p1.swift
@@ -1,1 +1,30 @@
+import UIKit
+
+enum NumberError: Error{
+    case negative
+    var message:String{
+        switch self {
+        case .negative: return "ERROR: Number can not be Negative"
+        }
+    }
+}
+func checkno(no:Int) throws{
+    if(no<0){
+        throw NumberError.negative
+    }
+    else if(no%2==0){
+        print("Even")
+    }
+    else{
+        print("Odd")
+    }
+}
+do {
+    try checkno(no: 5)
+}
+catch{
+    if let error=error as? NumberError{
+        print(error.message)
+    }
+}
 
